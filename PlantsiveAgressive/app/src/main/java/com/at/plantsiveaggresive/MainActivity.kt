fe58ad1a5be80b596bd955e082d34f36fd002530
Tudor Alexa <tudor.alexa99@gmail.com>
2021-04-17 16:56:59 +0300

    Add layout

--- PlantsiveAgressive/app/src/main/java/com/at/plantsiveaggresive/MainActivity.kt
+++ PlantsiveAgressive/app/src/main/java/com/at/plantsiveaggresive/MainActivity.kt
@@ -1,13 +1,27 @@
-    package com.at.plantsiveaggresive
+package com.at.plantsiveaggresive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.at.plantsiveaggresive.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActiviMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        database = Firebase.database.reference
+
+        database.child("")
     }
 }
